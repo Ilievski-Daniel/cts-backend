fd5d98454c5a6ed0f6818008e5e6bed7219e2ce9
Daniel Ilievski <ilievskiot@hotmail.com>
2023-06-10 09:55:51 +0200

    Update CI/CD Pipeline and fix linting issues on test_main.py

--- test_main.py
+++ test_main.py
@@ -1,12 +1,20 @@
+"""
+Module to test the functionality of the CTS Backend.
+"""
+
 import unittest
 from main import create_app
 
 class TestCTSBackend(unittest.TestCase):
+    """Test case for CTS Backend."""
+
     def setUp(self):
+        """Set up the test case."""
         self.app = create_app()
         self.client = self.app.test_client()
 
     def test_ping_endpoint(self):
+        """Test the '/ping' endpoint."""
         response = self.client.get('/ping')
         self.assertEqual(response.status_code, 200)
         data = response.get_json()
